446ba5142d265a8a7438e1634188f7e540bd487e
Prowderypulp <fedchs@icloud.com>
2025-08-02 13:21:11 +0530

    Updated backend files

--- backend/civicbackend/civicbackend/settings.py
+++ backend/civicbackend/civicbackend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'issues',
 ]
 
 MIDDLEWARE = [
@@ -70,13 +71,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'civicbackend.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# PostgreSQL Database Configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'genomix_db',
+        'USER': 'genomix_user',
+        'PASSWORD': 'chinmay10', # Use environment variables in production
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
